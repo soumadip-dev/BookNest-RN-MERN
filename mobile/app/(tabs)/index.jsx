2960475306a15be59be983717edf3fcb77909158
soumadip-dev <soumadipmajila@gmail.com>
2025-03-01 14:20:33 +0530

    feat: implemented function renderItem in index.jsx

--- mobile/app/(tabs)/index.jsx
+++ mobile/app/(tabs)/index.jsx
@@ -14,22 +14,76 @@ import { useEffect, useState } from 'react';
 import styles from '../../assets/styles/home.styles';
 import { API_URL } from '../../constants/api';
 import { Ionicons } from '@expo/vector-icons';
-// import { formatPublishDate } from '../../lib/utils';
+// import { formatPublishDate } from "../../lib/utils";
 import COLORS from '../../constants/colors';
-// import Loader from '../../components/Loader';
+// import Loader from "../../components/Loader";
 
-const Home = () => {
+export const sleep = ms => new Promise(resolve => setTimeout(resolve, ms));
+
+function Home() {
   const { token } = useAuthStore();
   const [books, setBooks] = useState([]);
   const [loading, setLoading] = useState(true);
   const [refreshing, setRefreshing] = useState(false);
   const [page, setPage] = useState(1);
   const [hasMore, setHasMore] = useState(true);
-  
-  return (
-    <View style={styles.container}>
-      <Text>index</Text>
+
+  useEffect(() => {
+    if (token) {
+      fetchBooks();
+    }
+  }, [token]);
+
+  const handleLoadMore = async () => {};
+
+  const renderItem = ({ item }) => (
+    <View style={styles.bookCard}>
+      <View style={styles.bookHeader}>
+        <View style={styles.userInfo}>
+          <Image source={{ uri: item.user.profileImage }} style={styles.avatar} />
+          <Text style={styles.username}>{item.user.username}</Text>
+        </View>
+      </View>
+
+      <View style={styles.bookImageContainer}>
+        <Image source={item.image} style={styles.bookImage} contentFit="cover" />
+      </View>
+
+      <View style={styles.bookDetails}>
+        <Text style={styles.bookTitle}>{item.title}</Text>
+        <View style={styles.ratingContainer}>{renderRatingStars(item.rating)}</View>
+        <Text style={styles.caption}>{item.caption}</Text>
+        {/* <Text style={styles.date}>Shared on {formatPublishDate(item.createdAt)}</Text> */}
+      </View>
     </View>
   );
-};
+
+  const renderRatingStars = rating => {
+    const stars = [];
+    for (let i = 1; i <= 5; i++) {
+      stars.push(
+        <Ionicons
+          key={i}
+          name={i <= rating ? 'star' : 'star-outline'}
+          size={16}
+          color={i <= rating ? '#f4b400' : COLORS.textSecondary}
+          style={{ marginRight: 2 }}
+        />
+      );
+    }
+    return stars;
+  };
+
+  return (
+    <View style={styles.container}>
+      <FlatList
+        data={books}
+        renderItem={renderItem}
+        keyExtractor={item => item._id}
+        contentContainerStyle={styles.listContainer}
+        showsVerticalScrollIndicator={false}
+      />
+    </View>
+  );
+}
 export default Home;
