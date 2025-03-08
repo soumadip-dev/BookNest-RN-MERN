758bac8d5411883dcb3f20e1c7377d44e5555fc9
soumadip-dev <soumadipmajila@gmail.com>
2025-03-08 09:25:33 +0530

    chore: add all necessary imports in profile page

--- mobile/app/(tabs)/profile.jsx
+++ mobile/app/(tabs)/profile.jsx
@@ -1,7 +1,28 @@
-import { View, Text } from 'react-native';
+import { useEffect, useState } from 'react';
+import {
+  View,
+  Alert,
+  Text,
+  FlatList,
+  TouchableOpacity,
+  ActivityIndicator,
+  RefreshControl,
+} from 'react-native';
+import { useRouter } from 'expo-router';
+import { API_URL } from '../../constants/api';
+import { useAuthStore } from '../../store/authStore';
+import styles from '../../assets/styles/profile.styles';
+// import ProfileHeader from '../../components/ProfileHeader';
+// import LogoutButton from '../../components/LogoutButton';
+import { Ionicons } from '@expo/vector-icons';
+import COLORS from '../../constants/colors';
+import { Image } from 'expo-image';
+import { sleep } from '.';
+import Loader from '../../components/Loader';
+
 const Profile = () => {
   return (
-    <View>
+    <View style={styles.container}>
       <Text>profile</Text>
     </View>
   );
