b5ece13d1a267ae996738b0d020604a0408179e7
soumadip-dev <soumadipmajila@gmail.com>
2025-04-15 19:40:19 +0530

    chore: update root _layout with new changes

--- mobile/app/_layout.jsx
+++ mobile/app/_layout.jsx
@@ -1,6 +1,7 @@
 import { Stack } from 'expo-router';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import SafeScreen from '../components/SafeScreen';
+import { StatusBar } from 'react-native';
 
 export default function RootLayout() {
   return (
@@ -11,6 +12,7 @@ export default function RootLayout() {
           <Stack.Screen name="(auth)" />
         </Stack>
       </SafeScreen>
+      <StatusBar style="dark" />
     </SafeAreaProvider>
   );
 }
