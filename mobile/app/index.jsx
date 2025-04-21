e6e62f1a0ead93b13d2362f417414573694605dc
soumadip-dev <soumadipmajila@gmail.com>
2025-04-21 08:15:42 +0530

    chore: temporarily display userName from authStore in index.jsx

--- mobile/app/index.jsx
+++ mobile/app/index.jsx
@@ -1,7 +1,15 @@
 import { Text, View } from 'react-native';
 import { Link } from 'expo-router';
+import { useAuthStore } from '../store/authStore';
+import { useEffect } from 'react';
 
 export default function Index() {
+  const { user, token, checkAuth } = useAuthStore();
+
+  useEffect(() => {
+    checkAuth();
+  }, []);
+
   return (
     <View
       style={{
@@ -10,6 +18,9 @@ export default function Index() {
         alignItems: 'center',
       }}
     >
+      {user && (
+        <Text style={{ fontSize: 20, fontWeight: 'bold', color: 'blue' }}>{user.username}</Text>
+      )}
       <Text style={{ fontSize: 20, fontWeight: 'bold' }}>
         <Link href="/(auth)/signup">Signup Page</Link>
       </Text>
