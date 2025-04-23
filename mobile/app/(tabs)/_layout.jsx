3c5e4f2b0b196bdffe36630f49b536ad3c3f8bc3
soumadip-dev <soumadipmajila@gmail.com>
2025-04-23 14:30:18 +0530

    fix: error in tabs fixed

--- mobile/app/(tabs)/_layout.jsx
+++ mobile/app/(tabs)/_layout.jsx
@@ -1,61 +1,58 @@
-import { View, Text } from 'react-native';
-import { Tabs } from 'expo-router';
-import { Ionicons } from '@expo/vector-icons';
-import COLORS from '../../constants/colors';
-import { useSafeAreaInsets } from 'react-native-safe-area-context';
+import { Tabs } from "expo-router";
+import { Ionicons } from "@expo/vector-icons";
+import COLORS from "../../constants/colors";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
 
-const TabLayout = () => {
+export default function TabLayout() {
   const insets = useSafeAreaInsets();
+
   return (
-    <View>
-      <Tabs
-        screenOptions={{
-          headerShown: false,
-          tabBarActiveTintColor: COLORS.primary,
-          headerTitleStyle: {
-            color: COLORS.textPrimary,
-            fontWeight: '600',
-          },
-          headerShadowVisible: false,
-          tabBarStyle: {
-            backgroundColor: COLORS.cardBackground,
-            borderTopWidth: 1,
-            borderTopColor: COLORS.border,
-            paddingTop: 5,
-            paddingBottom: insets.bottom,
-            height: 60 + insets.bottom,
-          },
+    <Tabs
+      screenOptions={{
+        headerShown: false,
+        tabBarActiveTintColor: COLORS.primary,
+        headerTitleStyle: {
+          color: COLORS.textPrimary,
+          fontWeight: "600",
+        },
+        headerShadowVisible: false,
+        tabBarStyle: {
+          backgroundColor: COLORS.cardBackground,
+          borderTopWidth: 1,
+          borderTopColor: COLORS.border,
+          paddingTop: 5,
+          paddingBottom: insets.bottom,
+          height: 60 + insets.bottom,
+        },
+      }}
+    >
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: "Home",
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="home-outline" size={size} color={color} />
+          ),
         }}
-      >
-        <Tabs.Screen
-          name="index"
-          options={{
-            title: 'Home',
-            tabBarIcon: ({ color, size }) => (
-              <Ionicons name="home-outline" size={size} color={color} />
-            ),
-          }}
-        />
-        <Tabs.Screen
-          name="create"
-          options={{
-            title: 'Create',
-            tabBarIcon: ({ color, size }) => (
-              <Ionicons name="add-circle-outline" size={size} color={color} />
-            ),
-          }}
-        />
-        <Tabs.Screen
-          name="profile"
-          options={{
-            title: 'Profile',
-            tabBarIcon: ({ color, size }) => (
-              <Ionicons name="person-outline" size={size} color={color} />
-            ),
-          }}
-        />
-      </Tabs>
-    </View>
+      />
+      <Tabs.Screen
+        name="create"
+        options={{
+          title: "Create",
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="add-circle-outline" size={size} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="profile"
+        options={{
+          title: "Profile",
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="person-outline" size={size} color={color} />
+          ),
+        }}
+      />
+    </Tabs>
   );
-};
-export default TabLayout;
+}
