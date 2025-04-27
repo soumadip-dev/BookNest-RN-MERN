20acc729e6cf90a4243fb9dc129a3de2be706a63
soumadip-dev <soumadipmajila@gmail.com>
2025-04-27 15:35:47 +0530

    add needed imports in index.jsx

--- mobile/app/(tabs)/index.jsx
+++ mobile/app/(tabs)/index.jsx
@@ -1,7 +1,26 @@
-import { View, Text } from 'react-native';
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  FlatList,
+  ActivityIndicator,
+  RefreshControl,
+} from 'react-native';
+import { useAuthStore } from '../../store/authStore';
+
+import { Image } from 'expo-image';
+import { useEffect, useState } from 'react';
+
+import styles from '../../assets/styles/home.styles';
+import { API_URL } from '../../constants/api';
+import { Ionicons } from '@expo/vector-icons';
+// import { formatPublishDate } from '../../lib/utils';
+import COLORS from '../../constants/colors';
+// import Loader from '../../components/Loader';
+
 const Home = () => {
   return (
-    <View>
+    <View style={styles.container}>
       <Text>index</Text>
     </View>
   );
