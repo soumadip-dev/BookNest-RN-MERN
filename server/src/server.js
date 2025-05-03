460dc4d92546868fb625b7a58c4bc5307cdca549
soumadip-dev <soumadipmajila@gmail.com>
2025-05-03 10:33:47 +0530

    feat: add home route in server.js to return welcome message

--- server/src/server.js
+++ server/src/server.js
@@ -9,7 +9,10 @@ const PORT = ENV.PORT || 8080;
 app.use(express.json()); // for parsing application/json
 app.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 
-
+//* Home route
+app.get('/', (req, res) => {
+  res.send('Hello from the BookNest server');
+});
 
 app.listen(PORT, () => {
   console.info(`✔️ Server is up and running on port: ${PORT}`);
