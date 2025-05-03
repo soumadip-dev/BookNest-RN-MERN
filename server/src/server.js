ed072b7d2b4c00c4c9e803fbfd0e84918dab1219
soumadip-dev <soumadipmajila@gmail.com>
2025-05-03 13:15:28 +0530

    feat: imported PORT from ENV and use it in server.js

--- server/src/server.js
+++ server/src/server.js
@@ -1,8 +1,9 @@
 import express from 'express';
+import { ENV } from './config/env.config';
 
 const app = express();
 
-const PORT = 8080;
+const PORT = ENV.PORT || 8080;
 
 app.listen(PORT, () => {
   console.info(`✔️ Server is up and running on port: ${PORT}`);
