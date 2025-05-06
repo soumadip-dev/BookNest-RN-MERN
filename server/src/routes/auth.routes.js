af305bf8833e272450f298e76ff0498cc713cdb5
soumadip-dev <soumadipmajila@gmail.com>
2025-05-06 13:45:39 +0530

    feat: import auth controllers and define routes in auth.routes.js

--- server/src/routes/auth.routes.js
+++ server/src/routes/auth.routes.js
@@ -1,5 +1,12 @@
 import { Router } from 'express';
+import { registerUser, loginUser } from '../controller/auth.controller.js';
 
+//* Create a new Express router
 const router = Router();
 
+//* Define routes
+router.post('/register', registerUser);
+router.post('/login', loginUser);
+
+//* Export the router
 export default router;
