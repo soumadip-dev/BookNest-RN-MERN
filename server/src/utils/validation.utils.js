e52a4c647d5f970e2dc8439f1973c4f2ced8fdfc
soumadip-dev <soumadipmajila@gmail.com>
2025-05-06 17:25:14 +0530

    feat: add generateToken utility function for JWT creation

--- server/src/utils/validation.utils.js
+++ server/src/utils/validation.utils.js
@@ -1,3 +1,5 @@
+import { ENV } from '../config/env.config.js';
+
 //* Email Regex Validation function
 export const isValidEmail = email => {
   const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
@@ -14,4 +16,9 @@ export const isStrongPassword = password => {
   // .{6,}              -> must be at least 6 characters long
   const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^A-Za-z0-9]).{6,}$/;
   return passwordRegex.test(password);
+};
+
+//* Generate JWT token function
+export const generateToken = userId => {
+  return jwt.sign({ userId }, ENV.JWT_SECRET, { expiresIn: '15d' });
 };
