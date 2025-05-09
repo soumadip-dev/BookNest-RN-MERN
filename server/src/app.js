18649c5f3909e9f7ef42a199af6d71ae1ed55fe8
soumadip-dev <soumadipmajila@gmail.com>
2025-05-09 13:45:28 +0530

    feat: added book route in app.js

--- server/src/app.js
+++ server/src/app.js
@@ -21,6 +21,9 @@ app.post('/api/v1/health', (req, res) => {
 //* Auth route
 app.use('/api/v1/auth', authRoutes);
 
+//* Book route
+app.use('/api/v1/book', bookRoutes);
+
 //* Book Route
 app.use('/api/v1/book', bookRoutes);
 
