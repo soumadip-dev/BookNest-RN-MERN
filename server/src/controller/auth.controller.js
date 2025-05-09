d7cf430928c8b97ef60c078588a6d58c0aeb5b1b
soumadip-dev <soumadipmajila@gmail.com>
2025-05-09 09:18:33 +0530

    feat: add controller to login user

--- server/src/controller/auth.controller.js
+++ server/src/controller/auth.controller.js
@@ -1,4 +1,6 @@
+import User from '../models/User.model.js';
 import { registerService } from '../services/auth.service.js';
+import { generateToken, isValidEmail } from '../utils/helper.utils.js';
 
 //* Controller to register a new user
 const registerUser = async (req, res) => {
@@ -32,7 +34,67 @@ const registerUser = async (req, res) => {
 };
 
 const loginUser = async (req, res) => {
-  // implement login logic
+  try {
+    // Get fields from request body
+    const { email, password } = req.body;
+
+    // Check if all fields are provided or not
+    if (!email || !password) {
+      return res.status(400).json({
+        message: 'All fields are required',
+        success: false,
+      });
+    }
+
+    // Check if email is valid or not
+    if (!isValidEmail(email)) {
+      return res.status(400).json({
+        message: 'Email is not valid',
+        success: false,
+      });
+    }
+
+    // Find the user based on email
+    const user = await User.findOne({ email });
+
+    // Check if user exists or not
+    if (!user) {
+      return res.status(400).json({
+        message: 'Invalid credentials',
+        success: false,
+      });
+    }
+
+    // Check if password is correct or not
+    const isPasswordCorrect = await user.comparePassword(password);
+    if (!isPasswordCorrect)
+      return res.status(400).json({
+        message: 'Invalid credentials',
+        success: false,
+      });
+
+    const token = generateToken(user._id);
+
+    // Send success response
+    res.status(200).json({
+      token,
+      user: {
+        id: user._id,
+        username: user.username,
+        email: user.email,
+        profileImage: user.profileImage,
+        createdAt: user.createdAt,
+      },
+      message: 'User logged in successfully',
+      success: true,
+    });
+  } catch (error) {
+    console.error('Error in login route', error);
+    res.status(400).json({
+      message: error.message || 'Something went wrong',
+      success: false,
+    });
+  }
 };
 
 export { registerUser, loginUser };
