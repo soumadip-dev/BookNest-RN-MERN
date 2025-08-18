b73bbe1efd42ba2f9ae9ea6a630e21ea113132f5
soumadip-dev <soumadipmajila@gmail.com>
2025-08-18 20:57:18 +0530

    feat: initialize server.js with DB connection and server startup logic

--- server/src/server.js
+++ server/src/server.js
@@ -1,8 +1,18 @@
 import app from './app.js';
+import { connectDB } from './config/db.config.js';
 import { ENV } from './config/env.config.js';
 
 const PORT = ENV.PORT || 8080;
 
-app.listen(PORT, () => {
-  console.info(`✔️ Server is up and running on port: ${PORT}`);
-});
+//* Function to connect DB and start server
+const startServer = async () => {
+  try {
+    await connectDB(); // Ensure DB is ready before server starts
+    app.listen(PORT, () => console.info(`✔️ Server is up and running on port: ${PORT}`));
+  } catch (error) {
+    console.error('❌ Failed to start server:', error.message);
+    process.exit(1);
+  }
+};
+
+startServer();
