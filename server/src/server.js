1e990571cdd778d3ffa55609d1d0b5106fac19d3
soumadip-dev <soumadipmajila@gmail.com>
2025-08-18 20:19:51 +0530

    feat: add middleware for JSON and URL-encoded request parsing

--- server/src/server.js
+++ server/src/server.js
@@ -5,6 +5,12 @@ const app = express();
 
 const PORT = ENV.PORT || 8080;
 
+//* Middlewares
+app.use(express.json()); // for parsing application/json
+app.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+
+
+
 app.listen(PORT, () => {
   console.info(`✔️ Server is up and running on port: ${PORT}`);
 });
