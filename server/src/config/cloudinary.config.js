acf6bb1ce13adb4ed88b3fd17cbbff88f634ddb6
soumadip-dev <soumadipmajila@gmail.com>
2025-08-19 21:58:11 +0530

    fix: correct Cloudinary configuration file

--- server/src/config/cloudinary.config.js
+++ server/src/config/cloudinary.config.js
@@ -2,12 +2,10 @@ import { v2 as cloudinary } from 'cloudinary';
 import { ENV } from './env.config.js';
 
 //* Configure Cloudinary client
-const connectCloudinary = async () => {
-  cloudinary.config({
-    cloud_name: ENV.CLOUDINARY_CLOUD_NAME,
-    api_key: ENV.CLOUDINARY_API_KEY,
-    api_secret: ENV.CLOUDINARY_API_SECRET,
-  });
-};
+cloudinary.config({
+  cloud_name: ENV.CLOUDINARY_CLOUD_NAME,
+  api_key: ENV.CLOUDINARY_API_KEY,
+  api_secret: ENV.CLOUDINARY_API_SECRET,
+});
 
-export default connectCloudinary;
+export default cloudinary;
