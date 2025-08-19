ce0f70e1851e063a90b2d2a93d1d538ed2b22583
soumadip-dev <soumadipmajila@gmail.com>
2025-08-19 23:27:05 +0530

    feat: apply auth middleware to protect book creation route

--- server/src/routes/book.routes.js
+++ server/src/routes/book.routes.js
@@ -1,11 +1,12 @@
 import { Router } from 'express';
 import { createBook } from '../controller/book.controller.js';
+import { protectRoute } from '../middleware/auth.middleware.js';
 
 //* Create a new Express router
 const router = Router();
 
 //* Define routes
-router.post('/', createBook);
+router.post('/', protectRoute, createBook);
 
 //* Export the router
 export default router;
