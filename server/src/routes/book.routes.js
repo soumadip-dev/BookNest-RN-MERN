4e718ab866df01bc7c73f6925b54131c393a07ef
soumadip-dev <soumadipmajila@gmail.com>
2025-08-19 21:49:41 +0530

    feat: add router for book-related endpoints

--- server/src/routes/book.routes.js
+++ server/src/routes/book.routes.js
@@ -1,5 +1,11 @@
 import { Router } from 'express';
+import { createBook } from '../controller/book.controller.js';
 
+//* Create a new Express router
 const router = Router();
 
+//* Define routes
+router.post('/', createBook);
+
+//* Export the router
 export default router;
