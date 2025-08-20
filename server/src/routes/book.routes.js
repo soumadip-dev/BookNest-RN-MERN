0fb85e5af295d270e79cb1456c671f5fa5888d68
soumadip-dev <soumadipmajila@gmail.com>
2025-08-20 22:09:29 +0530

    feat: add route for fetching recommended books

--- server/src/routes/book.routes.js
+++ server/src/routes/book.routes.js
@@ -1,5 +1,10 @@
 import { Router } from 'express';
-import { createBook, getAllBooks, deleteBook } from '../controller/book.controller.js';
+import {
+  createBook,
+  getAllBooks,
+  deleteBook,
+  getRecommendedBooks,
+} from '../controller/book.controller.js';
 import { protectRoute } from '../middleware/auth.middleware.js';
 
 //* Create a new Express router
@@ -9,6 +14,7 @@ const router = Router();
 router.post('/', protectRoute, createBook);
 router.get('/', protectRoute, getAllBooks);
 router.delete('/:id', protectRoute, deleteBook);
+router.get('/user', getRecommendedBooks);
 
 //* Export the router
 export default router;
