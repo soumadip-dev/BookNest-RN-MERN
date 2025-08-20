57a2fa58ea2a250ad021cc5bfd35bc0d5ddc7cf8
soumadip-dev <soumadipmajila@gmail.com>
2025-08-20 20:44:35 +0530

    feat: add paginated books endpoint with query params and user population

--- server/src/controller/book.controller.js
+++ server/src/controller/book.controller.js
@@ -1,3 +1,4 @@
+import Book from '../models/Book.model.js';
 import { createBookService } from '../services/book.service.js';
 
 //* Controller to create a new Book
@@ -24,4 +25,41 @@ const createBook = async (req, res) => {
   }
 };
 
-export { createBook };
+//* Controller to get all books (paginated)
+const getAllBooks = async (req, res) => {
+  try {
+    // Get the page number and limit from query parameters
+    const page = parseInt(req.query.page) || 1;
+    const limit = parseInt(req.query.limit) || 5;
+
+    // Calculate the skip value
+    const skip = (page - 1) * limit;
+
+    // Get all books
+    const books = await Book.find()
+      .skip(skip)
+      .limit(limit)
+      .populate('user', 'username profileImage');
+
+    // Calculate total books
+    const totalBooks = await Book.countDocuments();
+
+    // Send success response with pagination data
+    res.status(200).json({
+      books,
+      totalBooks,
+      currentOage: page,
+      totalPages: Math.ceil(totalBooks / limit),
+      message: 'Books fetched successfully',
+      success: true,
+    });
+  } catch (error) {
+    console.error('Error in get all books route', error);
+    res.status(400).json({
+      message: error.message || 'Something went wrong',
+      success: false,
+    });
+  }
+};
+
+export { createBook, getAllBooks };
