4a9cb104d82b01eea12e0622e8c822f551162a04
soumadip-dev <soumadipmajila@gmail.com>
2025-08-20 21:30:57 +0530

    feat: add controller to delete book by id with ownership and Cloudinary cleanup

--- server/src/controller/book.controller.js
+++ server/src/controller/book.controller.js
@@ -53,4 +53,48 @@ const getAllBooks = async (req, res) => {
   }
 };
 
-export { createBook, getAllBooks };
+//* Controller to delete all books
+const deleteBook = async (req, res) => {
+  try {
+    // Get the id from request params
+    const { id } = req.params;
+
+    // Check if id is provided or not
+    if (!id) {
+      throw new Error('Id is required');
+    }
+
+    // find the book by id
+    const book = await Book.findById(id);
+
+    // Check if book is found or not
+    if (!book) {
+      throw new Error('Book not found');
+    }
+
+    // Check if the user is the owner of the book or not
+    if (book.user.toString() !== req.user._id.toString()) {
+      throw new Error('Unauthorized');
+    }
+
+    // Delete the image from cloudinary
+    if (book.image && book.image.includes('cloudinary')) {
+      try {
+        const publicId = book.image.split('/').pop().split('.')[0];
+        await cloudinary.uploader.destroy(publicId);
+      } catch (delError) {
+        console.error('Error in deleting image from cloudinary', delError);
+      }
+    }
+    // Delete the book
+    await book.deleteOne();
+
+    // Send success response
+    res.status(200).json({ message: 'All books deleted successfully', success: true });
+  } catch (error) {
+    console.error('Error in delete all books route', error);
+    res.status(400).json({ message: error.message || 'Something went wrong', success: false });
+  }
+};
+
+export { createBook, getAllBooks, deleteBook };
